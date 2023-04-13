2d8f2916d857492cdd9972378d2e40e23e25fe2e
lucass2k <lucasnahuel2000silva@gmail.com>
2023-04-12 22:20:53 -0300

    ejercicio 9

--- Practicas1Ej9.pas
+++ Practicas1Ej9.pas
@@ -5,3 +5,24 @@ con la secuencia de números, e imprimir el resultado final.
 Por ejemplo:
 ○ Si se lee el carácter “-” y la secuencia 4 3 5 -6 0 , deberá imprimir: 2 (4 – 3 – 5 - (-6) )
 ○ Si se lee el carácter “+” y la secuencia -10 5 6 -1 0, deberá imprimir 0 ( -10 + 5 + 6 + (-1) )}
+program ej9;
+var
+ signo: char;
+ num1,num2,num3,num4,total: integer;
+begin
+    writeln('ingrese + o -');read(signo);
+	if(signo <> '+') and (signo <> '-') then
+	write('se esperaba un caracter + o -')
+	else 
+	if(signo = '+') then
+	begin
+	read(num1,num2,num3,num4);
+	write(num1+num2+num3+num4);
+	end
+else
+	if(signo = '-') then
+	begin
+	read(num1,num2,num3,num4);
+	write(num1-num2-num3-num4);
+	end
+end.
